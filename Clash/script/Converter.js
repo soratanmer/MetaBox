25b9f55608157e41b560886458b23b87b9525144
soratanmer <soratanmer@gmail.com>
2024-09-24 23:47:17 +0800

    update clash meta converter

---
+++ Clash/script/Converter.js
@@ -0,0 +1,25 @@
+const { name } = $arguments
+
+let config = JSON.parse($files[0])
+
+let proxyProviders = {}
+
+$substore.cache.subs.map(sub => {
+    if (sub.name === name) {
+        proxyProviders = {
+            [sub.name]: {
+                type: "http",
+                interval: 86400,
+                "health-check": {
+                    enable: true,
+                    url: "https://www.gstatic.com/generate_204",
+                    interval: 300
+                },
+                url: sub.url,
+                path: `./proxies/${sub.name}.yaml`
+            }
+        }
+    }
+})
+
+$content = JSON.stringify(config, null, 4)
